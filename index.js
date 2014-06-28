1717241d35801429ead92f22e7e6627dbc6682a1
William Blankenship <william.jblankenship@gmail.com>
2014-06-28 16:33:27 -0500

    Let there be code

---
+++ index.js
@@ -0,0 +1,42 @@
+var fs = require('fs')
+var os = require('os')
+
+module.exports = function getOs(cb) {
+  if(os.platform() == "linux") {
+    getLinuxDistro(cb)
+  }
+  return cb(null, os.platform())
+}
+
+function getLinuxDistro(cb) {
+  fs.readFile("os.json",function(e,data) {
+    if(e) return cb(e)
+    var distros = JSON.parse(data)
+    getReleaseFile(Object.keys(distros),function(e,file) {
+      var candidates = distros[file]
+      fs.readFile(file,'utf-8',function(e,file) {
+        file = file.toLowerCase()
+        if(candidates.length===1) return cb(null,candidates[0])
+        candidates.forEach(function(candidate) {
+          candidate = candidate.split(" ")[0].toLowerCase()
+          if(file.indexOf(candidate)>=0) return cb(null,candidate)
+        })
+      })
+    })()
+  })
+}
+
+function getReleaseFile(names,cb) {
+  var index = 0
+  return function checkExists() {
+    fs.stat(names[index],function(e,stat) {
+      if(e || !stat.isFile()) {
+        console.log(names[index]+" not found!")
+        index++
+        return checkExists()
+      }
+      if(names.length <= index) return cb(new Error("No file unique file found!"))
+      cb(null,names[index])
+    })
+  }
+}
