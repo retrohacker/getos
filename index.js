fd227ecc171614e6aa9a0968784b6e71dc5a07a4
William Blankenship <william.jblankenship@gmail.com>
2014-06-29 00:43:51 -0500

    Requires in os.json and cachesDistro for subsequent calls. Closes #1

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 var fs = require('fs')
 var os = require('os')
+var distros = require('./os.json')
+var cachedDistro = null
 
 module.exports = function getOs(cb) {
   if(os.platform() == "linux") {
-    getLinuxDistro(cb)
+    return getLinuxDistro(cb)
   }
   return cb(null, os.platform())
 }
 
 function getLinuxDistro(cb) {
-  fs.readFile("os.json",function(e,data) {
-    if(e) return cb(e)
-    var distros = JSON.parse(data)
-    getReleaseFile(Object.keys(distros),function(e,file) {
-      var candidates = distros[file]
-      fs.readFile(file,'utf-8',function(e,file) {
-        file = file.toLowerCase()
-        if(candidates.length===1) return cb(null,candidates[0])
-        candidates.forEach(function(candidate) {
-          candidate = candidate.split(" ")[0].toLowerCase()
-          if(file.indexOf(candidate)>=0) return cb(null,candidate)
-        })
+  if(cachedDistro) return cb(null,cachedDistro)
+  getReleaseFile(Object.keys(distros),function(e,file) {
+    var candidates = distros[file]
+    fs.readFile(file,'utf-8',function(e,file) {
+      file = file.toLowerCase()
+      if(candidates.length===1) {
+        cachedDistro = candidates[0]
+        return cb(null,candidates[0])
+      }
+      candidates.forEach(function(candidate) {
+        check = candidate.split(" ")[0].toLowerCase()
+        if(file.indexOf(check)>=0) {
+          cachedDistro = candidate
+          return cb(null,candidate)
+        }
       })
-    })()
-  })
+    })
+  })()
 }
 
 function getReleaseFile(names,cb) {
@@ -31,8 +36,8 @@ function getReleaseFile(names,cb) {
   return function checkExists() {
     fs.stat(names[index],function(e,stat) {
       if(e || !stat.isFile()) {
-        console.log(names[index]+" not found!")
         index++
+        console.log(names[index]+" not found!")
         return checkExists()
       }
       if(names.length <= index) return cb(new Error("No file unique file found!"))
