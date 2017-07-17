75e827ada4d87c540c278b57824a526d1eab1d51
Aidan Harris <mail@aidanharris.io>
2017-07-17 16:38:43 +0100

    Use path.resolve and ignore EEXIST error

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const fs = require('fs')
 const platform = require('os').platform()
 const symlinks = require('./symlinks.json')
@@ -7,14 +8,14 @@ const COPY_FILES = (platform === 'win32')
 
 Object.keys(symlinks).forEach(function (key) {
   if (COPY_FILES) {
-    fs.readFile(__dirname + '/' + symlinks[key], function (err, data) {
+    fs.readFile(path.join(__dirname, symlinks[key]), function (err, data) {
       // Log the error and move on...
       if (err) {
         console.error(err)
         return
       }
 
-      fs.writeFile(__dirname + '/' + key, data, function (err) {
+      fs.writeFile(path.join(__dirname, key), data, function (err) {
         if (err) {
           console.error(err)
         }
@@ -22,8 +23,8 @@ Object.keys(symlinks).forEach(function (key) {
     })
     return
   }
-  fs.symlink(__dirname + '/' + symlinks[key], __dirname + '/' + key, function (err) {
-    if (err) {
+  fs.symlink(path.join(__dirname, symlinks[key]), path.join(__dirname, key), function (err) {
+    if (err && err.code !== 'EEXIST') {
       console.error(err)
     }
   })
